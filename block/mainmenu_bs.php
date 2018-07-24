9877ec51b38e26659d648ca6f810b29275905950
giorgi <giorgi199817@gmail.com>
2018-07-25 01:16:02 +0400

    NOTIFICATION PAGE

--- block/mainmenu_bs.php
+++ block/mainmenu_bs.php
@@ -1,3 +1,41 @@
+<!-- // DAMATEBULI -->
+<span style="float:right; font-size:10px; color:#999; margin-right:15px;">
+  <?php
+$currentFile = $_SERVER["SCRIPT_NAME"];
+  $parts = Explode('/', $currentFile);
+  $currentFile = $parts[count($parts) - 1];
+if (session_status() == PHP_SESSION_NONE) {
+  session_start();
+}
+if(!isset($_SESSION['loggedin']))
+{
+header('Location: ../login.php');
+  die("To access this page, you need to <a href='../login.php'>LOGIN</a>"); // Make sure they are logged in!
+} // What the !isset() code does, is check to see if the variable $_SESSION['loggedin'] is there, and if it isn't it kills the script telling the user to log in!
+
+
+
+if($currentFile!="products.php" and isset($_SESSION['loggedin']) and isset($_SESSION['departmentName']))
+die("მხოლოდ ადმინისტრატორებს შეუძლიათ ამ გვერდის ნახვა"); // Make sure they are logged in!
+
+if(isset($_SESSION['loggedin']) and !isset($_SESSION['departmentName']))
+$isAdmin = true;
+else
+$isAdmin = false;
+?>
+
+
+<style type="text/css">
+<!--
+.colortext {
+color:	#999;
+font-size:12px;
+
+}
+
+</style>
+  </span>
+<!-- //////////////////////// -->
 <nav class="navbar navbar-expand-sm navbar-dark bg-dark" style="padding: .4rem 1rem;">
   <a class="navbar-brand" href="#">
     <img src="../images/iliauni_logo.png" class="d-inline-block align-top" alt="IESRESOURCE">
---
+++ staff/baratebiNEW.php
@@ -0,0 +1,187 @@
+﻿<?php
+include("../block/globalVariables.php");
+include("../block/db.php");
+include("../block/functions.php");
+include("../checklogin1.php");// amocmebs tu aris avtorizacia gavlili
+//if(!HaveAccess("seismicData")){echo CreatePageData($_POST," ../login.php"); exit();}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>დეპარტამენტი</title>
+<link rel="stylesheet" href="../block/bootstrap-4.1.1-dist/css/bootstrap.min.css">
+<link href="../block/custom-bs4-styles.css" rel="stylesheet" type="text/css"/>
+<link href="../block/fontawesome-free-5.1.0-web/css/all.css" rel="stylesheet">
+<!-- <link href="../block/style.css" rel="stylesheet" type="text/css"/> -->
+<!-- <script type='text/javascript' src='departmetns.js'></script> -->
+<style>
+	#content {
+		width: 90%;
+		height: 500px;
+		margin-top: 30px;
+	}
+	.alert {
+		margin-top: 10px;
+	}
+</style>
+</head>
+
+<body>
+<?php include("../block/mainmenu_bs.php");?>
+
+<?php
+if(isset($_GET['id'])) //რედაქტირება
+{
+	$tableLabel = "დეპარტამენტის რედაქტირება";
+	$btnLabel  = "რედაქტირება";
+	$id = $_GET['id'];
+	mysqli_select_db($db, $dbStaff);
+
+	$query = "SELECT * FROM departments WHERE id=$id";
+	$departments = mysqli_query($db, $query) or die($query);
+	$department = mysqli_fetch_array($departments);
+	$name = $department['name'];
+}else //დამატება
+{
+	$tableLabel = "დეპარტამენტის დამატება";
+	$btnLabel  = "დამატება";
+	$id = "";
+	$name = "";
+}
+?>
+
+
+<!-- Create Menu Settings: (Menu ID, Is Vertical, Show Timer, Hide Timer, On Click, Right to Left, Horizontal Subs, Flush Left) -->
+<script type="text/javascript">qm_create(0,false,0,250,false,false,false,false);</script>
+
+
+<?php
+mysqli_select_db($db, $dbStaff);
+////////////////////////////////
+if ($isAdmin)
+                {
+////////////////////////////////
+$currentDay = new DateTime(date("Y-m-d"));
+$currentDay->modify('-15 day');
+$minus15Day = $currentDay->format('Y-m-d');
+$currentDay->modify('-15 day');
+$minus30Day = $currentDay->format('Y-m-d');
+$query1 = "SELECT DATEDIFF(contract_end_date, NOW()) AS datedf, contract_end_date, id, first_name, last_name FROM staff WHERE DATEDIFF(contract_end_date, NOW()) < $firstWarningForContractEndDate AND DATEDIFF(contract_end_date, NOW()) >= $secondWarningForContractEndDate";
+$query2 = "SELECT DATEDIFF(contract_end_date, NOW()) AS datedf, contract_end_date, id, first_name, last_name FROM staff WHERE DATEDIFF(contract_end_date, NOW()) < $secondWarningForContractEndDate AND  DATEDIFF(contract_end_date, NOW()) > 0";
+$query3 = "SELECT DATEDIFF(contract_end_date, NOW()) AS datedf, contract_end_date, id, first_name, last_name FROM staff WHERE DATEDIFF(contract_end_date, NOW()) <= 0 AND DATEDIFF(contract_end_date, NOW()) > -$expDayForSalaryCardEndDate";
+$query4 = "SELECT DATEDIFF(salary_card_end_date, NOW()) AS datedf, salary_card_end_date, id, first_name, last_name FROM staff WHERE DATEDIFF(salary_card_end_date, NOW()) < $firstWarningForSalaryCardEndDate AND DATEDIFF(salary_card_end_date, NOW()) >= $secondWarningForSalaryCardEndDate";
+$query5 = "SELECT DATEDIFF(salary_card_end_date, NOW()) AS datedf, salary_card_end_date, id, first_name, last_name FROM staff WHERE DATEDIFF(salary_card_end_date, NOW()) < $secondWarningForSalaryCardEndDate AND  DATEDIFF(salary_card_end_date, NOW()) > 0";
+$query6 = "SELECT DATEDIFF(salary_card_end_date, NOW()) AS datedf, salary_card_end_date, id, first_name, last_name FROM staff WHERE DATEDIFF(salary_card_end_date, NOW()) <= 0 AND DATEDIFF(salary_card_end_date, NOW()) > -$expDayForSalaryCardEndDate";
+
+$table1 = mysqli_query($db, $query1);
+$table2 = mysqli_query($db, $query2);
+$table3 = mysqli_query($db, $query3);
+$table4 = mysqli_query($db, $query4);
+$table5 = mysqli_query($db, $query5);
+$table6 = mysqli_query($db, $query6);
+
+
+if(mysqli_num_rows($table1) > 0 or mysqli_num_rows($table2) > 0 or mysqli_num_rows($table3) > 0 or mysqli_num_rows($table4) > 0 or mysqli_num_rows($table5) > 0 or mysqli_num_rows($table6) > 0):
+?>
+<div id="content" class="container">
+<?php
+
+while($row = mysqli_fetch_array($table1))
+{
+	$id=$row["id"];
+	$contract_end_date = $row["contract_end_date"];
+	$first_name=$row["first_name"];
+	$last_name=$row["last_name"];
+	$datedf = $row["datedf"];
+ ?>
+<div class="alert alert-warning">
+ <?php echo "\"<a href='../staff/newEdit_staff.php?id=$id' class='linToUser'>".$first_name ." ".$last_name."</a>\"-ს ხელშეკრულების ვადა გადის ".$datedf." დღეში (".$contract_end_date." -ში)"; ?>
+</div>
+<?php
+}
+
+while($row = mysqli_fetch_array($table2))
+{
+	$id=$row["id"];
+	$contract_end_date = $row["contract_end_date"];
+	$first_name=$row["first_name"];
+	$last_name=$row["last_name"];
+	$datedf = $row["datedf"];
+ ?>
+<div class="alert alert-warning">
+ <?php echo "\"<a href='../staff/newEdit_staff.php?id=$id' class='linToUser'>".$first_name ." ".$last_name."</a>\"-ს ხელშეკრულების ვადა გადის ".$datedf." დღეში (".$contract_end_date." -ში)"; ?>
+</div>
+<?php
+}
+
+while($row = mysqli_fetch_array($table3))
+{
+	$id=$row["id"];
+	$contract_end_date = $row["contract_end_date"];
+	$first_name=$row["first_name"];
+	$last_name=$row["last_name"];
+	$datedf = $row["datedf"];
+ ?>
+<div class="alert alert-danger">
+ <?php echo "\"<a href='../staff/newEdit_staff.php?id=$id' class='linToUser'>".$first_name ." ".$last_name."</a>\"-ს ხელშეკრულებას ვადა გაუვიდა ".$contract_end_date." -ში"; ?>
+</div>
+<?php
+}
+
+while($row = mysqli_fetch_array($table4))
+{
+	$id=$row["id"];
+	$salary_card_end_date = $row["salary_card_end_date"];
+	$first_name=$row["first_name"];
+	$last_name=$row["last_name"];
+	$datedf = $row["datedf"];
+ ?>
+<div class="alert alert-warning">
+ <?php echo "\"<a href='../staff/newEdit_staff.php?id=$id' class='linToUser'>".$first_name ." ".$last_name."</a>\"-ს სახელფასო ბარათის ვადა გადის".$datedf." დღეში (".$salary_card_end_date." -ში)"; ?>
+</div>
+<?php
+}
+
+while($row = mysqli_fetch_array($table5))
+{
+	$id=$row["id"];
+	$salary_card_end_date = $row["salary_card_end_date"];
+	$first_name=$row["first_name"];
+	$last_name=$row["last_name"];
+	$datedf = $row["datedf"];
+ ?>
+<div class="alert alert-warning">
+ <?php echo "\"<a href='../staff/newEdit_staff.php?id=$id' class='linToUser'>".$first_name ." ".$last_name."</a>\"-ს სახელფასო ბარათის ვადა გადის ".$datedf." დღეში (".$salary_card_end_date." -ში)"; ?>
+</div>
+<?php
+}
+
+while($row = mysqli_fetch_array($table6))
+{
+	$id=$row["id"];
+	$salary_card_end_date = $row["salary_card_end_date"];
+	$first_name=$row["first_name"];
+	$last_name=$row["last_name"];
+	$datedf = $row["datedf"];
+ ?>
+<div class="alert alert-danger">
+ <?php echo "\"<a href='../staff/newEdit_staff.php?id=$id' class='linToUser'>".$first_name ." ".$last_name."</a>\"-ს სახელფასო ბარათს ვადა გაუვიდა ".$salary_card_end_date." -ში"; ?>
+</div>
+<?php
+}
+?>
+</div>
+<?php
+endif;
+mysqli_select_db($db, $dbInventari);
+				}
+?>
+
+
+
+
+
+
+</body>
+</html>
